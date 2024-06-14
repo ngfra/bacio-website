896ab5817250a7ec71c6c4982a1505d12ecbb52d
Hong Ngoc Frankl <ngocfrankl@gmail.com>
2024-06-14 21:42:35 +0200

    add head & nav.

---
+++ nav.php
@@ -0,0 +1,26 @@
+<div class="wrap">
+    <div class="nav-container">
+      <div class="container nav">
+        <nav class="container navbar navbar-expand-lg ">
+          <div id="nav" class="container align-items-end">
+            <a href="index.php"><img id="logo" src="imgs/bacio-logo.png" alt="bacio" title="bacio"></a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+              <div class="navbar-nav site-nav-link d-flex">
+                <a class="d-flex justify-content-center" aria-current="page" href="about.php">über bacio</a>
+                <a class="d-flex justify-content-center" href="pizzas.php">pizzen</a>
+                <a class="d-flex justify-content-center" href="accessories.php">zubehör</a>
+                <a class="d-flex justify-content-center" href="contact.php">kontakt</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+  </div>
