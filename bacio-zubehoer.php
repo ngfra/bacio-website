0a2bb59a1807f6b68937576445c29a61405d5475
Hong Ngoc Frankl <ngocfrankl@gmail.com>
2024-06-15 21:36:35 +0200

    add zubehör page.

---
+++ bacio-zubehoer.php
@@ -0,0 +1,68 @@
+<?php include('head.php'); ?>
+<title>BACIO | Zubehör</title>
+
+<body>
+  <!-- navigation -->
+  <?php include('nav.php')?>
+
+  <header>
+    <div class="container header-container">
+      <h1 class="titel-h1 mt-section text-center">für Dein BACIO-Erlebnis</h1>
+      <p class="font-header">Ergänze dein BACIO-Erlebnis mit unserem ausgewählten Zubehör, was du für den perfekten Genuss brauchst.</p>
+      <img class="header-item-right" width="250px" src="imgs/illustrations-svg/Pizza-story.svg" alt="frische Pilze">
+
+    </div>
+  </header>
+
+  <main>
+    <?php include('group-social.php')?>
+
+    <section class="mt-50">
+      <div class="container accesories-container">
+        <div class="row d-flex align-items-center ">
+          <div class="col-md-5 d-flex justify-content-center">
+            <img class="w-100" src="imgs/bacio-kochschürze.png" alt="bacio-kochschürze"
+            title="BCIO Kochschürze">
+          </div>
+          <div class="col-md-7">
+            <h2 class="titel-h3">bacio tasche</h2>
+            <p class="mt-h-t">Unsere stilvoller Kochschürze schützt nicht nur deine Kleidung, sondern lässt dich auch wie ein echte Pizzaiolo fühlen. Aus hochwertigem, langlebigem Material gefertigt und mit dem BACIO-Logo versehen, ist sie der perfekte Begleiter für deine Kochabenteuer! </p>
+          </div>
+        </div>
+      </div>
+
+      
+      <div class="container accesories-container ">
+        <div class="row d-flex align-items-center reverse-order-mobile">
+          <div class="col-md-7">
+            <h2 class="titel-h3">bacio Holzteller</h2>
+            <p class="mt-h-t">Serviere deine Pizza stilecht auf unserem edlen Holzteller mit eleganter Prägung. Dieser hochwertige Teller ist nicht nur ein Hingucker, sondern bringt auch ein Stück italienische Tradition auf deinen Tisch!</p>
+          </div>
+          <div class="col-md-5 d-flex justify-content-center">
+            <img class="w-100" src="imgs/bacio-holzteller.png" alt="bacio Holzteller"
+            title="BACIO Holzteller">
+          </div>
+        </div>
+      </div>
+
+      <div class="container accesories-container">
+        <div class="row d-flex align-items-center ">
+          <div class="col-md-5 d-flex justify-content-center">
+            <img class="w-100" src="imgs/bacio-tasche.png" alt="bacio-taschee"
+            title="BACIO Tasche">
+          </div>
+          <div class="col-md-7">
+            <h2 class="titel-h3 ">bacio tasche</h2>
+            <p class="mt-h-t">Trage deine BACIO-Liebe überallhin mit unserer nachhaltigen Stoff-Tote-Tasche. Aus recycelten Materialen gefertigt, bietet sie ausreichend Platz für all deine Einkäufe und setzt ein Zeichen für Umweltbewusstsein. Perfekt für den täglichen Gebrauch.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <?php include('footer.php') ?>
+  <!-- Bootstrap JS -->
+  <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
