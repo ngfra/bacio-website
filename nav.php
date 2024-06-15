68a0a4a8ecc3e317872062038e4732d4a59a4b38
Hong Ngoc Frankl <ngocfrankl@gmail.com>
2024-06-15 21:36:01 +0200

    update nav.

--- nav.php
+++ nav.php
@@ -5,22 +5,20 @@
           <div id="nav" class="container align-items-end p-0">
             <a href="index.php"><img id="logo" src="imgs/bacio-logo.png" alt="bacio" title="bacio"></a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div class="navbar-collapse justify-content-end collapse" id="navbarNavAltMarkup" style="">
               <div class="navbar-nav site-nav-link d-flex">
-                <a class="d-flex justify-content-center" aria-current="page" href="about.php">über bacio</a>
-                <a class="d-flex justify-content-center" href="pizzas.php">pizzen</a>
-                <a class="d-flex justify-content-center" href="accessories.php">zubehör</a>
+                <a class="d-flex justify-content-center" aria-current="page" href="ueber-bacio.php">über bacio</a>
+                <a class="d-flex justify-content-center" href="pizzen-bacio.php">pizzen</a>
+                <a class="d-flex justify-content-center" href="bacio-zubehoer.php">zubehör</a>
                 <a class="d-flex justify-content-center" href="contact.php">kontakt</a>
               </div>
             </div>
           </div>
         </nav>
-
       </div>
     </div>
   </div>
