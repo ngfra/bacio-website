c08c25045b4a125c635459f142706ca5f1ca4e48
Hong Ngoc Frankl <ngocfrankl@gmail.com>
2024-06-18 14:22:35 +0200

    add danke php.

---
+++ danke.php
@@ -0,0 +1,32 @@
+<?php include('head.php'); ?>
+<title>BACIO | datenschutz</title>
+
+<body>
+  <!-- navigation -->
+  <?php include('nav.php')?>
+  <div class="">
+    <header>
+      <div class="container header-container">
+        <h1 class="titel-h1 mt-section text-center">Dankeschön</h1>
+      </div>
+    </header>
+
+    <main>
+      <?php include('group-social.php')?>
+
+      <section class="mt-section extra-container ">
+        <div class="container ">
+          <p class="text-center font-header ">
+            Dein „Rezept“ ist auf dem Weg! Unsere Pizzaboten sind bereits unterwegs, um es sicher an den richtigen Ort
+            zu bringen. 🍕📬 <br>Falls erforderlich, werden wir uns bei dir melden!</p>
+
+          <div class="wrap d-flex justify-content-center"><a class="btn-secondary" href="index.php">zurück zur Home</a>
+          </div>
+        </div>
+
+      </section>
+    </main>
+  </div>
+</body>
+
+</html>
