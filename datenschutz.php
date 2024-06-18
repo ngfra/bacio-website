958f5116d215aa7fcf13c57c864fbf351c9d12fe
Hong Ngoc Frankl <ngocfrankl@gmail.com>
2024-06-18 14:21:40 +0200

    add datenschutz & impressum pages.

---
+++ datenschutz.php
@@ -0,0 +1,134 @@
+<?php include('head.php'); ?>
+<title>BACIO | datenschutz</title>
+
+<body>
+  <!-- navigation -->
+  <?php include('nav.php')?>
+
+  <header>
+    <div class="container header-container">
+      <h1 class="titel-h1 mt-section text-center">datenschutz</h1>
+    </div>
+  </header>
+
+  <main>
+    <?php include('group-social.php')?>
+
+    <section class="mt-section">
+      <div class="container text-container">
+        <h2 class="titel-h4">Kontaktdaten des Verantwortlichen
+        </h2>
+        <p>Sollten Sie Fragen zum Datenschutz oder zur Verarbeitung personenbezogener Daten haben, finden Sie
+          nachfolgend die Kontaktdaten der verantwortlichen Person bzw. Stelle: <br>
+          Bacio GmbH <br>
+          Innstraße 11 <br>
+          6020 Innsbruck <br>
+          Österreich <br>
+          Vertretungsberechtigt: Armando Ammirati <br>
+          E-Mail: office@musterfirma.com <br>
+          Telefon: 01234/56789 <br>
+          Impressum: pizzabacio.com <br>
+        </p>
+        <h2 class="titel-h4">Einleitung und Überblick</h2>
+        <p>Wir haben diese Datenschutzerklärung (Fassung 18.06.2024-122818254) verfasst, um Ihnen gemäß der Vorgaben der
+          Datenschutz-Grundverordnung (EU) 2016/679 und anwendbaren nationalen Gesetzen zu erklären, welche
+          personenbezogenen Daten (kurz Daten) wir als Verantwortliche – und die von uns beauftragten
+          Auftragsverarbeiter (z. B. Provider) – verarbeiten, zukünftig verarbeiten werden und welche rechtmäßigen
+          Möglichkeiten Sie haben. Die verwendeten Begriffe sind geschlechtsneutral zu verstehen. <br>
+          Kurz gesagt: Wir informieren Sie umfassend über Daten, die wir über Sie verarbeiten.</p>
+        <p>Datenschutzerklärungen klingen für gewöhnlich sehr technisch und verwenden juristische Fachbegriffe. Diese
+          Datenschutzerklärung soll Ihnen hingegen die wichtigsten Dinge so einfach und transparent wie möglich
+          beschreiben. Soweit es der Transparenz förderlich ist, werden technische Begriffe leserfreundlich erklärt,
+          Links zu weiterführenden Informationen geboten und Grafiken zum Einsatz gebracht. Wir informieren damit in
+          klarer und einfacher Sprache, dass wir im Rahmen unserer Geschäftstätigkeiten nur dann personenbezogene Daten
+          verarbeiten, wenn eine entsprechende gesetzliche Grundlage gegeben ist. Das ist sicher nicht möglich, wenn man
+          möglichst knappe, unklare und juristisch-technische Erklärungen abgibt, so wie sie im Internet oft Standard
+          sind, wenn es um Datenschutz geht. Ich hoffe, Sie finden die folgenden Erläuterungen interessant und
+          informativ und vielleicht ist die eine oder andere Information dabei, die Sie noch nicht kannten.</p>
+        <p>Wenn trotzdem Fragen bleiben, möchten wir Sie bitten, sich an die unten bzw. im Impressum genannte
+          verantwortliche Stelle zu wenden, den vorhandenen Links zu folgen und sich weitere Informationen auf
+          Drittseiten anzusehen. Unsere Kontaktdaten finden Sie selbstverständlich auch im Impressum.</p>
+
+        <h2 class="titel-h4">Anwendungsbereich</h2>
+        <p>Diese Datenschutzerklärung gilt für alle von uns im Unternehmen verarbeiteten personenbezogenen Daten und für
+          alle personenbezogenen Daten, die von uns beauftragte Firmen (Auftragsverarbeiter) verarbeiten. Mit
+          personenbezogenen Daten meinen wir Informationen im Sinne des Art. 4 Nr. 1 DSGVO wie zum Beispiel Name,
+          E-Mail-Adresse und postalische Anschrift einer Person. Die Verarbeitung personenbezogener Daten sorgt dafür,
+          dass wir unsere Dienstleistungen und Produkte anbieten und abrechnen können, sei es online oder offline. Der
+          Anwendungsbereich dieser Datenschutzerklärung umfasst:
+        <ul>
+          <li>alle Onlineauftritte (Websites, Onlineshops), die wir betreiben
+          </li>
+          <li>Social Media Auftritte und E-Mail-Kommunikation
+          </li>
+          <li>mobile Apps für Smartphones und andere Geräte
+          </li>
+        </ul>
+        Kurz gesagt: Die Datenschutzerklärung gilt für alle Bereiche, in denen personenbezogene Daten im Unternehmen
+        über die genannten Kanäle strukturiert verarbeitet werden. Sollten wir außerhalb dieser Kanäle mit Ihnen in
+        Rechtsbeziehungen eintreten, werden wir Sie gegebenenfalls gesondert informieren.
+        </p>
+
+        <h2 class="titel-h4">Rechtsgrundlagen </h2>
+        <p>In der folgenden Datenschutzerklärung geben wir Ihnen transparente Informationen zu den rechtlichen
+          Grundsätzen und Vorschriften, also den Rechtsgrundlagen der Datenschutz-Grundverordnung, die uns ermöglichen,
+          personenbezogene Daten zu verarbeiten. <br>
+          Was das EU-Recht betrifft, beziehen wir uns auf die VERORDNUNG (EU) 2016/679 DES EUROPÄISCHEN PARLAMENTS UND
+          DES RATES vom 27. April 2016. Diese Datenschutz-Grundverordnung der EU können Sie selbstverständlich online
+          auf EUR-Lex, dem Zugang zum EU-Recht, unter
+          https://eur-lex.europa.eu/legal-content/DE/ALL/?uri=celex%3A32016R0679 nachlesen.</p>
+        <ul>Wir verarbeiten Ihre Daten nur, wenn mindestens eine der folgenden Bedingungen zutrifft: <br>
+          <lo>1. Einwilligung (Artikel 6 Absatz 1 lit. a DSGVO): Sie haben uns Ihre Einwilligung gegeben, Daten zu einem
+            bestimmten Zweck zu verarbeiten. Ein Beispiel wäre die Speicherung Ihrer eingegebenen Daten eines
+            Kontaktformulars.</lo> <br><br>
+          <lo>2. Vertrag (Artikel 6 Absatz 1 lit. b DSGVO): Um einen Vertrag oder vorvertragliche Verpflichtungen mit
+            Ihnen zu erfüllen, verarbeiten wir Ihre Daten. Wenn wir zum Beispiel einen Kaufvertrag mit Ihnen
+            abschließen, benötigen wir vorab personenbezogene Informationen.</lo> <br><br>
+          <lo>3. Rechtliche Verpflichtung (Artikel 6 Absatz 1 lit. c DSGVO): Wenn wir einer rechtlichen Verpflichtung
+            unterliegen, verarbeiten wir Ihre Daten. Zum Beispiel sind wir gesetzlich verpflichtet Rechnungen für die
+            Buchhaltung aufzuheben. Diese enthalten in der Regel personenbezogene Daten.</lo> <br><br>
+          <lo>4. Berechtigte Interessen (Artikel 6 Absatz 1 lit. f DSGVO): Im Falle berechtigter Interessen, die Ihre
+            Grundrechte nicht einschränken, behalten wir uns die Verarbeitung personenbezogener Daten vor. Wir müssen
+            zum Beispiel gewisse Daten verarbeiten, um unsere Website sicher und wirtschaftlich effizient betreiben zu
+            können. Diese Verarbeitung ist somit ein berechtigtes Interesse.</lo>
+        </ul>
+
+        <p>Weitere Bedingungen wie die Wahrnehmung von Aufnahmen im öffentlichen Interesse und Ausübung öffentlicher
+          Gewalt sowie dem Schutz lebenswichtiger Interessen treten bei uns in der Regel nicht auf. Soweit eine solche
+          Rechtsgrundlage doch einschlägig sein sollte, wird diese an der entsprechenden Stelle ausgewiesen.</p>
+        <p>Zusätzlich zu der EU-Verordnung gelten auch noch nationale Gesetze:</p>
+        <ul>
+          <li>In Österreich ist dies das Bundesgesetz zum Schutz natürlicher Personen bei der Verarbeitung
+            personenbezogener Daten (Datenschutzgesetz), kurz DSG.</li>
+          <li>
+            In Deutschland gilt das Bundesdatenschutzgesetz, kurz BDSG.</li>
+        </ul>
+        <p>Sofern weitere regionale oder nationale Gesetze zur Anwendung kommen, informieren wir Sie in den folgenden
+          Abschnitten darüber.</p>
+
+          <h2 class="titel-h4">Speicherdauer </h2>
+      <p>Dass wir personenbezogene Daten nur so lange speichern, wie es für die Bereitstellung unserer Dienstleistungen
+        und Produkte unbedingt notwendig ist, gilt als generelles Kriterium bei uns. Das bedeutet, dass wir
+        personenbezogene Daten löschen, sobald der Grund für die Datenverarbeitung nicht mehr vorhanden ist. In einigen
+        Fällen sind wir gesetzlich dazu verpflichtet, bestimmte Daten auch nach Wegfall des ursprüngliches Zwecks zu
+        speichern, zum Beispiel zu Zwecken der Buchführung. <br>
+
+        Sollten Sie die Löschung Ihrer Daten wünschen oder die Einwilligung zur Datenverarbeitung widerrufen, werden die
+        Daten so rasch wie möglich und soweit keine Pflicht zur Speicherung besteht, gelöscht. <br>
+
+        Über die konkrete Dauer der jeweiligen Datenverarbeitung informieren wir Sie weiter unten, sofern wir weitere
+        Informationen dazu haben.
+
+      </p>
+      </div>
+
+    
+    </section>
+  </main>
+
+  <?php include('footer.php') ?>
+
+
+</body>
+
+</html>
---
+++ impressum.php
@@ -0,0 +1,121 @@
+<?php include('head.php'); ?>
+<title>BACIO | Impressum</title>
+
+<body>
+  <!-- navigation -->
+  <?php include('nav.php')?>
+
+  <header>
+    <div class="container header-container">
+      <h1 class="titel-h1 mt-section text-center">impressum</h1>
+    </div>
+  </header>
+
+  <main>
+    <?php include('group-social.php')?>
+
+    <section class="mt-section">
+      <div class="container text-container">
+        <h2 class="titel-h4">impressum</h2>
+        <p>
+          <strong>Pizza BACIO</strong> <br>
+          Innstraße 11<br>
+          6020 Innsbruck <br>
+          Österreich
+        </p>
+
+        <h2 class="titel-h4">Kontaktdaten </h2>
+        <p>
+          <strong>Tel.:</strong> <a class="text-decoration-none color-main" href="tel:+43 0645 63450911 ">+43 0645
+            63450911 </a> <br>
+          <strong>Fax:</strong> <a class="text-decoration-none color-main" href="tel:+43 0645 63450911 ">+43 0645
+            63450911 </a> <br>
+          <strong>E-Mail:</strong> <a class="text-decoration-none color-main"
+            href="mailto:contact@pizzabacio.com">contact@pizzabacio.com</a>
+        </p>
+
+        <p><strong>Firmenbuchnummer:</strong> 123456a <br>
+          <strong>Firmenbuchgericht: </strong>Innsbruck Gerichtstand <br>
+          <strong>Firmensitz</strong>: Innstraße WIFI Campus <br>
+          <strong>Unternehmensgegenstand:</strong> Lebensmittel <br>
+          <strong>UID-Nummer:</strong> ATU12345678 <br>
+          <strong>Mitglied bei:</strong> WKO, Landesinnung <br>
+        </p>
+
+        <p>
+          Aufsichtsbehörde/Gewerbebehörde: Bezirkshauptmannschaft Innsbruck <br>
+          Webseite: https://www.aufsichtsbeoerde-musterhausen.at/ <br>
+          Anschrift <br>
+          Haus A <br>
+          Landesmuseum Straße, 6020 Innsbruck <br>
+          Österreich <br>
+
+          Berufsbezeichnung: Gastgewerbe <br>
+          Verleihungsstaat: Österreich <br>
+
+          Persönlich haftende Gesellschafter (Komplementär): Armando Ammirati <br>
+          Geschäftsführer: Armando Ammirati <br>
+
+          Datenschutz Vertretungsberechtigter: Armando Ammirati <br>
+          E-Mail der Datenschutzstelle: office@musterfirma.com <br>
+          Tel.: 01234/56789 <br>
+          Impressum der Datenschutzstelle: https://www.testfirma.at/impressum/ <br>
+          Kontakt: <br>
+          Bacio GmbH <br>
+          Innstraße 11 <br>
+          6020 Innsbruck <br>
+          Österreich br
+        </p>
+
+        <h2 class="titel-h4">Haftung für Inhalte dieser Website</h2>
+        <p>Wir entwickeln die Inhalte dieser Website ständig weiter und bemühen uns korrekte und aktuelle Informationen
+          bereitzustellen. Leider können wir keine Haftung für die Korrektheit aller Inhalte auf dieser Website
+          übernehmen, speziell für jene, die seitens Dritter bereitgestellt wurden. Als Diensteanbieter sind wir nicht
+          verpflichtet, die von Ihnen übermittelten oder gespeicherten Informationen zu überwachen oder nach Umständen
+          zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. <br>
+
+          Unsere Verpflichtungen zur Entfernung von Informationen oder zur Sperrung der Nutzung von Informationen nach
+          den allgemeinen Gesetzen aufgrund von gerichtlichen oder behördlichen Anordnungen bleiben auch im Falle
+          unserer Nichtverantwortlichkeit davon unberührt. <br>
+
+          Sollten Ihnen problematische oder rechtswidrige Inhalte auffallen, bitte wir Sie uns umgehend zu kontaktieren,
+          damit wir die rechtswidrigen Inhalte entfernen können. Sie finden die Kontaktdaten im Impressum.
+        </p>
+
+        <h2 class="titel-h4">Haftung für Links auf dieser Website</h2>
+        <p>Unsere Website enthält Links zu anderen Websites für deren Inhalt wir nicht verantwortlich sind. Haftung für
+          verlinkte Websites besteht für uns nicht, da wir keine Kenntnis rechtswidriger Tätigkeiten hatten und haben,
+          uns solche Rechtswidrigkeiten auch bisher nicht aufgefallen sind und wir Links sofort entfernen würden, wenn
+          uns Rechtswidrigkeiten bekannt werden. <br>
+
+          Wenn Ihnen rechtswidrige Links auf unserer Website auffallen, bitte wir Sie uns zu kontaktieren. Sie finden
+          die Kontaktdaten im Impressum.
+        </p>
+
+        <h2 class="titel-h4">Urheberrechtshinweis</h2>
+        <p>Alle Inhalte dieser Webseite (Bilder, Fotos, Texte, Videos) unterliegen dem Urheberrecht. Bitte fragen Sie
+          uns bevor Sie die Inhalte dieser Website verbreiten, vervielfältigen oder verwerten wie zum Beispiel auf
+          anderen Websites erneut veröffentlichen. Falls notwendig, werden wir die unerlaubte Nutzung von Teilen der
+          Inhalte unserer Seite rechtlich verfolgen. <br>
+
+          Sollten Sie auf dieser Webseite Inhalte finden, die das Urheberrecht verletzen, bitten wir Sie uns zu
+          kontaktieren.
+
+        </p>
+
+        <h2 class="titel-h4">Bildernachweis</h2>
+        <p>Die Bilder, Fotos und Grafiken auf dieser Webseite sind urheberrechtlich geschützt. <br>
+        Alle Texte sind urheberrechtlich geschützt. <br></p>
+
+        <p>Quelle: Erstellt mit dem Impressum Generator von AdSimple</p>
+      </div>
+
+    </section>
+  </main>
+
+  <?php include('footer.php') ?>
+  <!-- Bootstrap JS -->
+  <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
