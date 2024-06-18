362ac71adcaa9b8605e4a8cad800f655a3059256
Hong Ngoc Frankl <ngocfrankl@gmail.com>
2024-06-18 14:19:25 +0200

    update height of social icons.

--- footer.php
+++ footer.php
@@ -5,9 +5,9 @@
           <div class="d-flex gap-2 align-items-end">
             <a href="index.php"><img id="logo" src="imgs/bacio-logo.png" alt="bacio" title="bacio"></a>
 
-            <a  target="_blank" href="https://www.facebook.com/profile.php?id=61560862052310"><img class="footer-social" height="45px"
+            <a  target="_blank" href="https://www.facebook.com/profile.php?id=61560862052310"><img class="footer-social" height="40px"
                 src="imgs/illustrations-svg/facebook-icon-red.svg" alt="instagram"></a>
-            <a target="_blank" href="https://www.instagram.com/bacio.pizza/"><img  class="footer-social"  height="45px"
+            <a target="_blank" href="https://www.instagram.com/bacio.pizza/"><img  class="footer-social"  height="40px"
                 src="imgs/illustrations-svg/insta-icon-red.svg" alt="instagram"></a>
           </div>
 
